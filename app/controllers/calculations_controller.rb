c7ee2929e650bcf24736d7256c7587c307052d51
Alex <ababich@asu.edu>
2014-06-21 13:43:43 -0700

    Fixed algebraic expressions, added boolean for player correctness, need to tie in to JS

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,27 +1,34 @@
 class CalculationsController < ApplicationController
     def create
-      @sum = eval(params[:data])        
+      @sum = eval(params[:data])     
     end
   
     def index
+      @correct = false
       alphabet = ("a".."z").to_a
       randomLetter1 = alphabet.shuffle.sample
       
       randomNumber1 = 1 + rand(10)
       randomNumber2 = 1 + rand(10)     
       randomNumberMystery = 1 + rand(10)      
-      symbolChance = 1 + rand(50)
+      symbolChance = 1 + rand(100)
       
       if (symbolChance < 50)
         symbol = "-"
-        randomResult = (randomNumber1 * randomNumber2) - randomNumberMystery
+        randomResult = (randomNumber1 * randomNumberMystery) - randomNumber2
       end
       else if (symbolChance > 50)
         symbol = "+"
-        randomResult = (randomNumber1 * randomNumber2) + randomNumberMystery
+        randomResult = (randomNumber1 * randomNumberMystery) + randomNumber2
       end
       
-      @result = randomResult.to_s
+      @result = randomResult
+      @missing = randomNumberMystery.to_s
+      
       @expression = randomNumber1.to_s + randomLetter1 + " " + symbol + " " + randomNumber2.to_s + " " + "=" + " " + randomResult.to_s
+      
+      if @sum == @missing
+        @correct = true
+      end
     end
 end
