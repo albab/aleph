02517d622f52f9a80916df76f3b4cb58d2f4577b
Alex <ababich@asu.edu>
2014-06-21 00:35:13 -0700

    Got random algebraic expressions to generate on the page

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,27 @@
 class CalculationsController < ApplicationController
-  def create
+    def create
       @sum = eval(params[:data])        
-  end
+    end
+  
+    def index
+      alphabet = ("a".."z").to_a
+      randomLetter1 = alphabet.shuffle.sample
+      
+      randomNumber1 = 1 + rand(10)
+      randomNumber2 = 1 + rand(10)     
+      randomNumberMystery = 1 + rand(10)      
+      symbolChance = 1 + rand(50)
+      
+      if (symbolChance < 50)
+        symbol = "-"
+        randomResult = (randomNumber1 * randomNumber2) - randomNumberMystery
+      end
+      else if (symbolChance > 50)
+        symbol = "+"
+        randomResult = (randomNumber1 * randomNumber2) + randomNumberMystery
+      end
+      
+      @result = randomResult.to_s
+      @expression = randomNumber1.to_s + randomLetter1 + " " + symbol + " " + randomNumber2.to_s + " " + "=" + " " + randomResult.to_s
+    end
 end
